aff8041b8c455d19d8a68b7fdda39baed10e8396
Alexander Perepechko <perepechko.alex@gmail.com>
2021-01-03 13:40:00 -0500

    Ranking is now properly counted. Added a new list and fixed some issues

--- index.js
+++ index.js
@@ -11,15 +11,20 @@ import fs from "fs";
 import { createFinalResultsTable, createTempTable } from "./lib/db/db.js";
 
 const outDir = "./data/out";
+let rankCount = 1;
 
 async function writeToFile(gameArr) {
-  gameArr = gameArr.filter((game) => !otherGames.includes(game.name));
+  // gameArr = gameArr.filter((game) => !otherGames.includes(game.name));
 
   gameArr = await gameArr.sort((a, b) =>
     a.totalScore > b.totalScore ? -1 : 1
   );
-  for (let i = 0; i < gameArr.length; i++) {
-    gameArr[i].finalRank = i + 1;
+
+  for (let game of gameArr) {
+    if (!otherGames.includes(game.name)) {
+      game.finalRank = rankCount;
+      rankCount++;
+    }
   }
 
   populateFinalResults(gameArr);
