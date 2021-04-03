29e595e6d9cb9989178a0c5cf4701cfea00be4fc
Alexander Perepechko <aperepechko@protonmail.com>
2021-04-03 01:33:57 -0400

    Modified directory structure to work correctly with middleware and server-middleware

--- api/index.js
+++ api/index.js
@@ -1,19 +1,30 @@
 import express from "express";
+import Nuxt from "nuxt";
+import Builder from "nuxt";
 const app = express();
 
-// Require API routes
-import * as apiRoutes from "../routes/routes.js";
+// Import and Set Nuxt.js options
+import * as config from "../nuxt.config.js";
+config.dev = !(process.env.NODE_ENV === "production");
 
-// Import API Routes
-app.use(apiRoutes);
+(async function () {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config);
 
-// Export the server middleware
-module.exports = {
-  path: "/api",
-  handler: app,
-};
+  const {
+    host = process.env.HOST || "127.0.0.1",
+    port = process.env.PORT || 3000,
+  } = nuxt.options.server;
 
-export default {
-  path: "/api",
-  handler: app,
-};
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt);
+    await builder.build();
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render);
+
+  // Listen the server
+  app.listen(port, host);
+})();
