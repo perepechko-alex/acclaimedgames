d88a9003c634179f8c00e02452ef324af6171150
Alexander Perepechko <aperepechko@protonmail.com>
2021-07-08 17:50:20 -0400

    Updated lastUpdated field

--- components/lastUpdated.vue
+++ components/lastUpdated.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <span id="last-updated" style="font-weight: bold; color: #cc0000">
-      List Last Updated On: July 01, 2021
+      List Last Updated On: July 08, 2021
     </span>
   </div>
 </template>
