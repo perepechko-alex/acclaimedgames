59155851e8c351fe12ce3ff5fa8147c162a3bd2e
Alexander Perepechko <aperepechko@protonmail.com>
2021-07-19 20:54:03 -0400

    Added new lists and modified lists for Street Fighter II in order to display the lists more easliy and correctly.

--- components/lastUpdated.vue
+++ components/lastUpdated.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <span id="last-updated" style="font-weight: bold; color: #cc0000">
-      List Last Updated On: July 08, 2021
+      List Last Updated On: July 19, 2021
     </span>
   </div>
 </template>
