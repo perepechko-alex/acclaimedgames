2a32f4abb151f34bc1f060aa76bea2378b5be8e1
Alexander Perepechko <aperepechko@protonmail.com>
2021-08-16 17:38:27 -0400

    Updated list with 2020 DICE GOTY

--- components/lastUpdated.vue
+++ components/lastUpdated.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <span id="last-updated" style="font-weight: bold; color: #cc0000">
-      List Last Updated On: July 19, 2021
+      List Last Updated On: August 16, 2021
     </span>
   </div>
 </template>
