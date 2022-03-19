44b1544f015d87a5bdbcdefc51dbcabdb11dcf66
alx <alx@alxexperience.fios-router.home>
2022-03-18 18:57:21 -0700

    Converted goat calculations and writes to python

--- lib/db.py
+++ lib/db.py
@@ -1,9 +1,11 @@
 import sqlite3
 
-conn = sqlite3.connect('./data/out/ggoat.db')
+
+def create_connection():
+    return sqlite3.connect('./data/out/ggoat.db')
 
 
-def create_final_results_table():
+def create_final_results_table(conn):
     conn.execute('DROP TABLE IF EXISTS "finalResults"')
     conn.execute('''CREATE TABLE IF NOT EXISTS "finalResults" (
       "rank"	INTEGER,
@@ -16,7 +18,7 @@ def create_final_results_table():
     )''')
 
 
-def create_views():
+def create_views(conn):
     conn.execute('''CREATE VIEW IF NOT EXISTS results_with_metadata AS
       SELECT finalResults.rank, finalResults.name, finalResults.totalScore, finalResults.numOfLists, finalResults.avgListYear, game_metadata.releasedate, game_metadata.platforms, game_metadata.developers FROM finalResults
       INNER JOIN game_metadata ON finalResults.name = game_metadata.name
@@ -27,7 +29,7 @@ def create_views():
       ORDER BY CASE WHEN finalResults.rank IS NULL THEN 1 ELSE 0 END''')
 
 
-def create_tables():
+def create_tables(conn):
     conn.execute('''CREATE TABLE IF NOT EXISTS "game_metadata" (
       "name"	TEXT NOT NULL,
       "releasedate"	TEXT,
@@ -49,7 +51,6 @@ def create_tables():
         PRIMARY KEY("filename","weightedpoints","name","publication", "listyear", "rank"),
         FOREIGN KEY("name") REFERENCES "game_metadata"("name"))''')
 
-
-create_tables()
-create_final_results_table()
-create_views()
+# create_tables()
+# create_final_results_table()
+# create_views()
