52fc1ed43d723f6b6545ff83fdf52c52a1c6763f
Alex Perepechko <aperepechko@protonmail.com>
2022-05-17 22:35:59 -0700

    Cleaned up formatting for db

--- lib/db.py
+++ lib/db.py
@@ -8,35 +8,35 @@ def create_connection():
 def create_final_results_table(conn):
     conn.execute('DROP TABLE IF EXISTS "finalResults"')
     conn.execute('''CREATE TABLE IF NOT EXISTS "finalResults" (
-      "rank"	INTEGER,
-      "name"	TEXT NOT NULL,
-      "totalscore"	NUMERIC NOT NULL,
-      "numoflists" INTEGER NOT NULL,
-      "avglistyear" INTEGER,
-      PRIMARY KEY("rank","name"),
-      FOREIGN KEY("name") REFERENCES "game_metadata"("name")
+        "rank"	INTEGER,
+        "name"	TEXT NOT NULL,
+        "totalscore"	NUMERIC NOT NULL,
+        "numoflists" INTEGER NOT NULL,
+        "avglistyear" INTEGER,
+        PRIMARY KEY("rank","name"),
+        FOREIGN KEY("name") REFERENCES "game_metadata"("name")
     )''')
 
 
 def create_views(conn):
     conn.execute('''CREATE VIEW IF NOT EXISTS results_with_metadata AS
-      SELECT finalResults.rank, finalResults.name, finalResults.totalScore, finalResults.numOfLists, finalResults.avgListYear, game_metadata.releasedate, game_metadata.platforms, game_metadata.developers FROM finalResults
-      INNER JOIN game_metadata ON finalResults.name = game_metadata.name
-      ORDER BY CASE WHEN finalResults.rank IS NULL THEN 1 ELSE 0 END, finalResults.rank''')
+        SELECT finalResults.rank, finalResults.name, finalResults.totalScore, finalResults.numOfLists, finalResults.avgListYear, game_metadata.releasedate, game_metadata.platforms, game_metadata.developers FROM finalResults
+        INNER JOIN game_metadata ON finalResults.name = game_metadata.name
+        ORDER BY CASE WHEN finalResults.rank IS NULL THEN 1 ELSE 0 END, finalResults.rank''')
     conn.execute('''CREATE VIEW IF NOT EXISTS results_with_metadata_1000 AS
-      SELECT finalResults.rank, finalResults.name, finalResults.totalScore, finalResults.numOfLists, finalResults.avgListYear, game_metadata.releasedate, game_metadata.platforms, game_metadata.developers FROM finalResults
-      INNER JOIN game_metadata ON finalResults.name = game_metadata.name  WHERE finalResults.rank <= 1000
-      ORDER BY CASE WHEN finalResults.rank IS NULL THEN 1 ELSE 0 END''')
+        SELECT finalResults.rank, finalResults.name, finalResults.totalScore, finalResults.numOfLists, finalResults.avgListYear, game_metadata.releasedate, game_metadata.platforms, game_metadata.developers FROM finalResults
+        INNER JOIN game_metadata ON finalResults.name = game_metadata.name  WHERE finalResults.rank <= 1000
+        ORDER BY CASE WHEN finalResults.rank IS NULL THEN 1 ELSE 0 END''')
 
 
 def create_tables(conn):
     conn.execute('''CREATE TABLE IF NOT EXISTS "game_metadata" (
-      "name"	TEXT NOT NULL,
-      "releasedate"	TEXT,
-      "platforms"	TEXT,
-      "developers" TEXT,
-      "genre" TEXT,
-      PRIMARY KEY("name"))''')
+        "name"	TEXT NOT NULL,
+        "releasedate"	TEXT,
+        "platforms"	TEXT,
+        "developers" TEXT,
+        "genre" TEXT,
+        PRIMARY KEY("name"))''')
     conn.execute('DROP TABLE IF EXISTS "goat"')
     conn.execute('''CREATE TABLE IF NOT EXISTS "goat" (
         "filename"	TEXT NOT NULL,
@@ -50,6 +50,7 @@ def create_tables(conn):
         "notes" TEXT,
         PRIMARY KEY("filename","weightedpoints","name","publication", "listyear", "rank"),
         FOREIGN KEY("name") REFERENCES "game_metadata"("name"))''')
+
 
 def create_all_tables(conn):
     create_tables(conn)
