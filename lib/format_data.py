af7e21eefa2d92edffcbd79b34c06eb5b116ef88
Alex Perepechko <aperepechko@protonmail.com>
2022-05-18 21:26:19 -0700

    Format and pull in the data that I need for metadata.

---
+++ lib/format_data.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+
+def format_platforms(game, platforms):
+    game['platforms'] = ','.join([str(platform['name']) for platform in platforms])
+    return game
+
+
+def format_genres(game, genres):
+    game['genres'] = ','.join([str(genre['name']) for genre in genres])
+    return game
+
+
+def format_year(games):
+    games_arr = []
+    for game in games:
+        if game.get('first_release_date') is not None:
+            game['first_release_date'] = datetime.fromtimestamp(game['first_release_date']).strftime('%Y')
+        games_arr.append(game)
+    return games_arr
+
+
+def format_devs(game, devs):
+    game['involved_companies'] = ','.join([str(dev['name']) for dev in devs])
+    return game
+
+
+def format_companies(companies):
+    companies_arr = []
+    for company in companies:
+        companies_arr.append(str(company['company']))
+    devs_joined_string = ','.join(companies_arr)
+    return devs_joined_string
+
--- lib/igdb.py
+++ lib/igdb.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from format_data import format_year, format_companies, format_genres, format_devs, format_platforms
+
 
 headers = {
     'Accept': 'application/json',
@@ -10,19 +12,13 @@ headers = {
 
 def get_games():
     url: str = 'https://api.igdb.com/v4/games'
-    data: str = 'fields name, genres, first_release_date, platforms; limit {}; offset {};'
-    limit: int = 2
+    data: str = 'fields name, genres, first_release_date, involved_companies, platforms; limit {}; offset {};'
+    limit: int = 5
     offset: int = 0
     all_games: list[dict] = []
     r = requests.post(url, data=data.format(limit, offset), headers=headers)
     all_games = all_games + [*r.json()]
     return all_games
-    # print(all_games)
-
-
-def format_platforms(game, platforms):
-    game['platforms'] = ','.join([str(platform['name']) for platform in platforms])
-    return game
 
 
 def get_platforms(games):
@@ -30,16 +26,62 @@ def get_platforms(games):
     data: str = 'fields name; where id = ({});'
     games_arr = []
     for game in games:
-        platform_string: list[str] = [str(n) for n in game['platforms']]
-        joined_string = ','.join(platform_string)
-        r = requests.post(url, data=data.format(joined_string), headers=headers)
-        platforms = r.json()
-        games_arr.append(format_platforms(game, platforms))
-    print(games_arr)
+        if game.get('platforms') is not None:
+            platform_string: list[str] = [str(n) for n in game['platforms']]
+            joined_string = ','.join(platform_string)
+            r = requests.post(url, data=data.format(joined_string), headers=headers)
+            platforms = r.json()
+            games_arr.append(format_platforms(game, platforms))
+        else:
+            games_arr.append(game)
+    return games_arr
 
 
-def get_formatted_games():
-    get_platforms(get_games())
+def get_genres(games):
+    url: str = 'https://api.igdb.com/v4/genres'
+    data: str = 'fields name; where id = ({});'
+    games_arr = []
+    for game in games:
+        if game.get('genres') is not None:
+            genre_string: list[str] = [str(n) for n in game['genres']]
+            joined_string = ','.join(genre_string)
+            r = requests.post(url, data=data.format(joined_string), headers=headers)
+            genres = r.json()
+            games_arr.append(format_genres(game, genres))
+        else:
+            games_arr.append(game)
+    return games_arr
 
 
-get_formatted_games()
+def get_devs(games):
+    involved_companies_url: str = 'https://api.igdb.com/v4/involved_companies'
+    devs_url: str = 'https://api.igdb.com/v4/companies'
+    involved_companies_data: str = 'fields company; where id = ({});'
+    devs_data: str = 'fields name; where id = ({});'
+    games_arr = []
+    for game in games:
+        if game.get('involved_companies') is not None:
+            companies: list[str] = [str(n) for n in game['involved_companies']]
+            companies_joined_string = ','.join(companies)
+            companies_req = requests.post(involved_companies_url,
+                                          data=involved_companies_data.format(companies_joined_string), headers=headers)
+
+            devs_joined_string = format_companies(companies_req.json())
+            devs_req = requests.post(devs_url, data=devs_data.format(devs_joined_string), headers=headers)
+            devs = devs_req.json()
+            games_arr.append(format_devs(game, devs))
+        else:
+            games_arr.append(game)
+    return games_arr
+
+
+def get_formatted_games(b, c, d, e):
+    def a(x):
+        return b(c(d(e(x))))
+
+    return a
+
+
+get_all_games = get_formatted_games(get_platforms, get_genres, format_year, get_devs)
+
+print(get_all_games(get_games()))
